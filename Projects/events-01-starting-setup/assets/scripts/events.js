3bcfff70f274304d65af769fe474e9b9541b38db
sivaram <sivaram3710@gmail.com>
2024-07-29 16:04:25 +0530

    version-2

--- Projects/events-01-starting-setup/assets/scripts/events.js
+++ Projects/events-01-starting-setup/assets/scripts/events.js
@@ -1,3 +1,21 @@
 const btn = document.querySelector("button");
-
 btn.addEventListener("click", (event) => console.log(event));
+
+// // To click a element in a list
+// const listItems = document.querySelectorAll("li");
+// listItems.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     event.target.classList.toggle("highlight");
+//   });
+// });
+
+const list = document.querySelector("ul");
+
+// we can remove the forEach loop by doing this method but if the list item has more than one element it fails
+list.addEventListener("click", (event) => {
+  event.target.classList.toggle("highlight");
+});
+
+list.addEventListener("click", (event) => {
+  event.target.closest("li").classList.toggle("highlight");
+});
