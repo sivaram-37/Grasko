5c1bb817564afa9eeb99030d23e9157b46f8c9f8
sivaram <sivaram3710@gmail.com>
2024-08-05 09:23:38 +0530

    version-2

--- Learning/Numbers/app.js
+++ Learning/Numbers/app.js
@@ -23,3 +23,13 @@ function randomNumberBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 console.log(randomNumberBetween(1, 3));
+
+// Generate 4 digit randam number
+const fourDigitRandomNumber = () => {
+  let a = Math.floor(Math.random() * 1e4).toString();
+  if (a.length === 3) {
+    a = 0 + a;
+  }
+  return a;
+};
+console.log(fourDigitRandomNumber());
