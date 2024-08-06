49b13266d2f39c447d1430fc05e67519767e9508
sivaram <sivaram3710@gmail.com>
2024-08-06 12:15:33 +0530

    version-1

--- Tooling/webpack.config.js
+++ Tooling/webpack.config.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-undef */
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/app.js",
   output: { filename: "app.js", path: path.resolve(__dirname, "assets", "scripts") },
+  devtool: "eval-cheap-module-source-map",
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
